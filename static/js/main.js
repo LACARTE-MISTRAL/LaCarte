ee77b826c105c2e9206c2aa189c7cd267ad99250
Sofía Huppertz <sofia.huppertz@gmail.com>
2024-05-25 17:14:34 +0200

    cards can be flipped and passed

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,60 @@
+
+
 document.addEventListener('DOMContentLoaded', event => {
 
     const initialDisplay = document.getElementById('initial');
     const flashCardsDisplay = document.getElementById('flash-cards');
-    const form = document.getElementById('generate-button');
-    const cardFront = document.getElementById('card-front-text');
-    const cardBack = document.getElementById('card-back-text');
+    const submitTextForm = document.getElementById('submit-text');
+    const cardContent = document.getElementById('card-content');
+    const flashCardsForm = document.getElementById('flash-cards-form');
+    let cards = [];
 
-    
-    form.addEventListener('submit', event =>
+
+    submitTextForm.addEventListener('submit', event =>
     {
         event.preventDefault();
         console.log("Event started");
         initialDisplay.style.display = 'none';
         flashCardsDisplay.style.display = 'block';
 
-        fetch (form.action, {
-            method: form.method,
-            body: new FormData(form)
+        fetch (submitTextForm.action, {
+            method: submitTextForm.method,
+            body: new FormData(submitTextForm)
         }).then(response => response.json())
         .then(data => {
-            console.log(data);
-            cardFront.innerHTML = data[0].front;
-            cardBack.innerHTML = data[0].back;
-            darcBack.style.display = 'none';
-
             
+            let cardIndex = 0;
+            let cards = data;
+            let maxCardValue = cards.length;
+
+            cardContent.innerHTML = data[cardIndex].front;
+            
+            flashCardsForm.addEventListener('click', event => {
+    
+                event.preventDefault();
+                if (event.target.value == 'prev' && cardIndex > 0)
+                {
+                    cardIndex--;
+                    cardContent.innerHTML = data[cardIndex].front;
+                }
+                else if (event.target.value == 'next' && cardIndex < maxCardValue - 1)        
+                {
+                    cardIndex++;
+                    cardContent.innerHTML = data[cardIndex].front;
+                }
+                else if(event.target.value == 'flip')
+                {
+                    if (cardContent.innerHTML == data[cardIndex].front)
+                    {
+                        cardContent.innerHTML = data[cardIndex].back;
+                    }
+                    else
+                    {
+                        cardContent.innerHTML = data[cardIndex].front;
+                    }
+                }
+                
+            });
         });
     });
 });
